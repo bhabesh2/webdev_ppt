a6a251b4801ca52f18f697c20dd76fac6f75e809
bhabesh2 <jsarma058@gmail.com>
2023-07-04 17:09:11 +0530

    done

---
+++ css2/Q8-onecard/script.js
@@ -0,0 +1,9 @@
+{
+  // this code is for humbugger toggle.
+  const hamburger = document.querySelector(".humbugger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    menu.classList.toggle("open");
+  });
+}

02d84589373ce54347b10f9b19edd1758a10c830
bhabesh2 <jsarma058@gmail.com>
2023-07-05 11:02:14 +0530

    done

---
+++ js-part1/q1.js
@@ -0,0 +1,19 @@
+
+/*
+hoisting in javascript.
+*/
+
+// in javascript hoisting works for only var as it has golbal scope. that menas this keyword can access value from any where. 
+
+var a = b;
+var b = 23;
+
+console.log(a + b) //a being undefined + b being 23 = NaN;
+
+var a;
+var b;
+
+a = b;
+b = 23;
+
+console.log(a + b); // 46
